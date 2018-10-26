a423e511b71711ad24e3c77bd22b10d32b5ff965
Sami Kurvinen <sami.kurvinen@gmail.com>
2018-10-26 22:11:36 +0300

    Decrement test

--- src/components/CounterSFC.vue
+++ src/components/CounterSFC.vue
@@ -1,7 +1,8 @@
 <template>
   <div>
     <div class="count">{{ count }}</div>
-    <button @click="count++">Increment</button>
+    <button @click="count++" class="increment">Increment</button>
+    <button @click="count > 0 && count--" class="decrement">Increment</button>
   </div>
 </template>
 
--- test/CounterSFC.spec.js
+++ test/CounterSFC.spec.js
@@ -16,9 +16,25 @@ describe('CounterSFC', () => {
   it('Increments the count when the button is clicked', () => {
     expect(wrapper.vm.count).toBe(0)
 
-    wrapper.find('button').trigger('click')
+    wrapper.find('.increment').trigger('click')
 
     expect(wrapper.vm.count).toBe(1)
+  })
+  
+  it('Decrements the count when the decrement button is clicked', () => {
+    expect(wrapper.vm.count).toBe(0)
+
+    wrapper.find('.increment').trigger('click') // 1 
+    wrapper.find('.increment').trigger('click') // 2
+    wrapper.find('.decrement').trigger('click') // 1
+
+    expect(wrapper.vm.count).toBe(1)
+  })
+  
+  it('Never goes below zero', () => {
+    wrapper.find('.decrement').trigger('click')
+    
+    expect(wrapper.vm.count).toBe(0)
   })
   
   it('Displays the current count', () => {
